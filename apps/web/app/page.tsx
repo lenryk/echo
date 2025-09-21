ea6f42266d416b12f06e86ce9341227c24b5cd16
lenryk <lenry101@gmail.com>
2025-09-21 14:06:42 +0100

    feat: add convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,21 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const add = useMutation(api.users.add);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">app/web</h1>
+        {users?.map((user) => (
+          <p key={user._id}>{user.name}</p>
+        ))}
+        <Button onClick={() => add()}>Add</Button>
       </div>
     </div>
-  )
+  );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@workspace/ui/components/button";
+"use client";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Widget!!</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">app/widget</h1>
+        {users?.map((user) => (
+          <p key={user._id}>{user.name}</p>
+        ))}
       </div>
     </div>
   );
