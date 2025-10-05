9c0896a6fa7854588c7e8d44ec157a38c4a268fa
lenryk <lenry101@gmail.com>
2025-10-05 19:54:13 +0100

    feat: add vapi plugin

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -43,4 +43,11 @@ export default defineSchema({
   users: defineTable({
     name: v.string(),
   }),
+  plugins: defineTable({
+    organizationId: v.string(),
+    service: v.union(v.literal("vapi")),
+    secretName: v.string(),
+  })
+    .index("by_organization_id", ["organizationId"])
+    .index("by_organization_id_and_service", ["organizationId", "service"]),
 });
