21d711225d18634093f3b11c12b1e58adb2ea03d
lenryk <lenry101@gmail.com>
2025-10-12 18:49:06 +0100

    feat: add embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+  DEFAULT_ORG_ID: "org_33C1PY8H5ma28S97GiYY3SCJV9G",
+  DEFAULT_POSITION: "bottom-right" as const,
+};
---
+++ apps/embed/eslint.config.js
@@ -0,0 +1,6 @@
+import baseConfig from "@workspace/eslint-config/base.js";
+
+export default {
+  ...baseConfig,
+  // Override or add any specific rules for this package
+};
